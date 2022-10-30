c01930af8659335b29c66501f6560406426aa6d6
Nick Benedetti <en.benedetti@pm.me>
2022-10-29 23:01:22 -0700

    Add bash script; test multiline

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,3 +1,18 @@
 #/bin/bash
 
 echo "test=mundo" >> $GITHUB_OUTPUT
+TEXT+="HELLO"'\n'
+TEXT+="WORLD"'\n'
+TEXT+="!"'\n'
+
+# https://stackoverflow.com/a/74137121
+message=$(echo $TEXT | tr '\n' ' ')
+echo "tr-no-newlines=$message" >> $GITHUB_OUTPUT
+
+# https://github.com/orgs/community/discussions/26288
+content=TEXT
+content="${content//'%'/'%25'}"
+content="${content//$'\n'/'%0A'}"
+content="${content//$'\r'/'%0D'}"
+
+echo "replace=$content" >> $GITHUB_OUTPUT
