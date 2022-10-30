e4d0c6c1f221df867eb5849ba3bf0433979bc638
Nick Benedetti <en.benedetti@pm.me>
2022-10-29 23:27:51 -0700

    echo outputs

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -7,12 +7,13 @@ TEXT+="!"'\n'
 echo -e $TEXT
 # https://stackoverflow.com/a/74137121
 message=$(echo $TEXT | tr '\n' ' ')
+echo $message
 echo "tr-no-newlines=$message" >> $GITHUB_OUTPUT
 
 # https://github.com/orgs/community/discussions/26288
-content=TEXT
+content=$TEXT
 content="${content//'%'/'%25'}"
 content="${content//$'\n'/'%0A'}"
 content="${content//$'\r'/'%0D'}"
-
+echo -e $TEXT
 echo "replace=$content" >> $GITHUB_OUTPUT
